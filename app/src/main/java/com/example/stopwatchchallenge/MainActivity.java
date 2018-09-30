2cdb0f520de8c386607c6b76d08af873cc4c04e7
davidrawlings <darawlings1@cougars.ccis.edu>
2018-09-30 13:21:28 -0500

    refactoring

--- app/src/main/java/com/example/stopwatchchallenge/MainActivity.java
+++ app/src/main/java/com/example/stopwatchchallenge/MainActivity.java
@@ -2,7 +2,6 @@ package com.example.stopwatchchallenge;
 
 import android.content.Context;
 import android.graphics.Color;
-import android.graphics.Typeface;
 import android.os.Handler;
 import android.os.SystemClock;
 import android.support.v7.app.AppCompatActivity;
@@ -15,15 +14,15 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button btnHome;
-    TextView textViewObject;
-    Handler customerHandler = new Handler();
+    Handler handler = new Handler();
     LinearLayout container;
     LinearLayout background;
+    Button btnHome;
+    TextView timerViewObject;
     TextView scoreViewObject;
     TextView attemptsViewObject;
 
-    long startTime=0L,timeInMilliseconds=0L; //,timeSwapBuff=0L,updateTime=0L;
+    long startTime=0L,timeInMilliseconds=0L;
 
     int score = 0;
     int attempts = 10;
@@ -36,9 +35,9 @@ public class MainActivity extends AppCompatActivity {
             int mins=secs/60;
             secs%=60;
             int milliseconds=(int)(timeInMilliseconds%1000/10);
-            textViewObject.setText(""+mins+":"+String.format("%02d",secs)+":"
+            timerViewObject.setText(""+mins+":"+String.format("%02d",secs)+":"
                                         +String.format("%02d",milliseconds));
-            customerHandler.postDelayed(this, 0);
+            handler.postDelayed(this, 0);
         }
     };
 
@@ -46,16 +45,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        btnHome = (Button)findViewById(R.id.btnHome);
-        textViewObject = (TextView)findViewById(R.id.timerView);
         container = (LinearLayout)findViewById(R.id.container);
         background = (LinearLayout)findViewById(R.id.background);
+        btnHome = (Button)findViewById(R.id.btnHome);
+        timerViewObject = (TextView)findViewById(R.id.timerView);
         scoreViewObject = (TextView)findViewById(R.id.scoreView);
         scoreViewObject.setText("Score   " + Integer.toString(score));
         attemptsViewObject = (TextView)findViewById(R.id.attemptsView);
         attemptsViewObject.setText("Attempts   " + Integer.toString(attempts));
-                startTime = SystemClock.uptimeMillis();
-                customerHandler.postDelayed(updateTimerThread, 0);
+        startTime = SystemClock.uptimeMillis();
+        handler.postDelayed(updateTimerThread, 0);
 
         background.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -63,15 +62,15 @@ public class MainActivity extends AppCompatActivity {
                 LayoutInflater inflater = (LayoutInflater)getBaseContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
                 View addView = inflater.inflate(R.layout.row, null);
 
-                TextView txtValue = (TextView)addView.findViewById(R.id.txtContent); // row
-                txtValue.setText(textViewObject.getText()); // main
-                Integer tenth = Integer.parseInt(textViewObject.getText().toString().substring(5, 6));
-                Integer hundredth = Integer.parseInt(textViewObject.getText().toString().substring(6, 7));
+                TextView rowViewObject = (TextView)addView.findViewById(R.id.rowView);
+                rowViewObject.setText(timerViewObject.getText());
+                Integer tenth = Integer.parseInt(timerViewObject.getText().toString().substring(5, 6));
+                Integer hundredth = Integer.parseInt(timerViewObject.getText().toString().substring(6, 7));
                 switch (tenth) {
                     case 0:
                     case 9:
                         if (tenth == 0 && hundredth == 0) {
-                            txtValue.setTextColor(Color.BLUE);
+                            rowViewObject.setTextColor(Color.BLUE);
                             score += 10;
                             attempts--;
                             scoreViewObject.setText("Score   " + Integer.toString(score));
@@ -79,7 +78,7 @@ public class MainActivity extends AppCompatActivity {
                         }
                         else {
                             //txtValue.setTextColor(Color.GREEN);
-                            txtValue.setTextColor(Color.rgb(0, 255, 0));
+                            rowViewObject.setTextColor(Color.rgb(0, 255, 0));
                             score += 5;
                             attempts--;
                             scoreViewObject.setText("Score   " + Integer.toString(score));
@@ -88,7 +87,7 @@ public class MainActivity extends AppCompatActivity {
                         break;
                     case 1:
                     case 8:
-                        txtValue.setTextColor(Color.rgb(255, 200, 0));
+                        rowViewObject.setTextColor(Color.rgb(255, 200, 0));
                         score += 3;
                         attempts--;
                         scoreViewObject.setText("Score   " + Integer.toString(score));
@@ -96,14 +95,14 @@ public class MainActivity extends AppCompatActivity {
                         break;
                     case 2:
                     case 7:
-                        txtValue.setTextColor(Color.rgb(255, 125, 0));
+                        rowViewObject.setTextColor(Color.rgb(255, 125, 0));
                         score += 1;
                         attempts--;
                         scoreViewObject.setText("Score   " + Integer.toString(score));
                         attemptsViewObject.setText("Attempts   " + Integer.toString(attempts));
                         break;
                     default:
-                        txtValue.setTextColor(Color.rgb(255, 0, 0));
+                        rowViewObject.setTextColor(Color.rgb(255, 0, 0));
                         break;
                 }
                 container.addView(addView, 0);
