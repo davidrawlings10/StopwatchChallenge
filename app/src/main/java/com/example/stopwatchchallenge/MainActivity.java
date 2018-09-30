5e4752106130dfb5743ea1cc6f99c256f5392006
davidrawlings <darawlings1@cougars.ccis.edu>
2018-09-30 14:00:18 -0500

    revert back to initial example to investigate issue

--- app/src/main/java/com/example/stopwatchchallenge/MainActivity.java
+++ app/src/main/java/com/example/stopwatchchallenge/MainActivity.java
@@ -12,32 +12,29 @@ import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
+import org.w3c.dom.Text;
+
 public class MainActivity extends AppCompatActivity {
 
-    Handler handler = new Handler();
+    Button btnStart, btnPause, btnLap;
+    TextView txtTimer;
+    Handler customHandler = new Handler();
     LinearLayout container;
-    LinearLayout background;
-    Button btnHome;
-    TextView timerViewObject;
-    TextView scoreViewObject;
-    TextView attemptsViewObject;
 
-    long startTime=0L,timeInMilliseconds=0L;
-
-    int score = 0;
-    int attempts = 10;
+    long startTime=0L,timeInMilliseconds=0L,timeSwapBuff=0L,updateTime=0L;
 
     Runnable updateTimerThread = new Runnable() {
         @Override
         public void run() {
             timeInMilliseconds = SystemClock.uptimeMillis()-startTime;
+            updateTime = timeSwapBuff+timeInMilliseconds;
             int secs=(int)(timeInMilliseconds/1000);
             int mins=secs/60;
             secs%=60;
-            int milliseconds=(int)(timeInMilliseconds%1000/10);
-            timerViewObject.setText(""+mins+":"+String.format("%02d",secs)+":"
-                                        +String.format("%02d",milliseconds));
-            handler.postDelayed(this, 0);
+            int milliseconds=(int)(updateTime%1000/10);
+            txtTimer.setText(""+mins+":"+String.format("%02d",secs)+":"
+                                        +String.format("%03d",milliseconds));
+            customHandler.postDelayed(this, 0);
         }
     };
 
@@ -45,67 +42,37 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        container = (LinearLayout)findViewById(R.id.container);
-        background = (LinearLayout)findViewById(R.id.background);
-        btnHome = (Button)findViewById(R.id.btnHome);
-        timerViewObject = (TextView)findViewById(R.id.timerView);
-        scoreViewObject = (TextView)findViewById(R.id.scoreView);
-        scoreViewObject.setText("Score   " + Integer.toString(score));
-        attemptsViewObject = (TextView)findViewById(R.id.attemptsView);
-        attemptsViewObject.setText("Attempts   " + Integer.toString(attempts));
-        startTime = SystemClock.uptimeMillis();
-        handler.postDelayed(updateTimerThread, 0);
 
-        background.setOnClickListener(new View.OnClickListener() {
+        btnStart = (Button)findViewById(R.id.btnStart);
+        btnPause = (Button)findViewById(R.id.btnPause);
+        btnLap = (Button)findViewById(R.id.btnLap);
+        txtTimer = (TextView)findViewById(R.id.timerValue);
+        container = (LinearLayout)findViewById(R.id.container);
+
+        btnStart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startTime = SystemClock.uptimeMillis();
+
+                customHandler.postDelayed(updateTimerThread,0);
+            }
+        });
+
+        btnPause.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                timeSwapBuff+=timeInMilliseconds;
+                customHandler.removeCallbacks(updateTimerThread);
+            }
+        });
+
+        btnLap.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 LayoutInflater inflater = (LayoutInflater)getBaseContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
                 View addView = inflater.inflate(R.layout.row, null);
-
-                TextView rowViewObject = (TextView)addView.findViewById(R.id.rowView);
-                rowViewObject.setText(timerViewObject.getText());
-                Integer tenth = Integer.parseInt(timerViewObject.getText().toString().substring(5, 6));
-                Integer hundredth = Integer.parseInt(timerViewObject.getText().toString().substring(6, 7));
-                switch (tenth) {
-                    case 0:
-                    case 9:
-                        if (tenth == 0 && hundredth == 0) {
-                            rowViewObject.setTextColor(Color.BLUE);
-                            score += 10;
-                            attempts--;
-                            scoreViewObject.setText("Score   " + Integer.toString(score));
-                            attemptsViewObject.setText("Attempts   " + Integer.toString(attempts));
-                        }
-                        else {
-                            //txtValue.setTextColor(Color.GREEN);
-                            rowViewObject.setTextColor(Color.rgb(0, 255, 0));
-                            score += 5;
-                            attempts--;
-                            scoreViewObject.setText("Score   " + Integer.toString(score));
-                            attemptsViewObject.setText("Attempts   " + Integer.toString(attempts));
-                        }
-                        break;
-                    case 1:
-                    case 8:
-                        rowViewObject.setTextColor(Color.rgb(255, 200, 0));
-                        score += 3;
-                        attempts--;
-                        scoreViewObject.setText("Score   " + Integer.toString(score));
-                        attemptsViewObject.setText("Attempts   " + Integer.toString(attempts));
-                        break;
-                    case 2:
-                    case 7:
-                        rowViewObject.setTextColor(Color.rgb(255, 125, 0));
-                        score += 1;
-                        attempts--;
-                        scoreViewObject.setText("Score   " + Integer.toString(score));
-                        attemptsViewObject.setText("Attempts   " + Integer.toString(attempts));
-                        break;
-                    default:
-                        rowViewObject.setTextColor(Color.rgb(255, 0, 0));
-                        break;
-                }
-                container.addView(addView, 0);
+                TextView txtValue = (TextView)addView.findViewById(R.id.txtContent);
+                container.addView(addView);
             }
         });
     }
